23f5a109428b3ae906c4c49710f389b6614eabca
Paul Christ <paul.l.christ@web.de>
2025-03-06 15:35:45 +0100

    Refactor: add LOOM

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 /**
+ * Export LOOM
+ */
+import LOOM from "@/LOOM/LOOM.vue";
+export { LOOM };
+
+/**
  * Export Base Component
  */
 import { BaseComponent } from "./components/BaseComponent/BaseComponent";
@@ -26,6 +32,10 @@ export type SerializedCARPETComponents =
   | SerializedButtonComponent
   | SerializedFormComponent
   | SerializedCodeEditorComponent;
+
+export interface SerialisedComponents {
+  [id: number]: SerializedCARPETComponents;
+}
 
 /**
  * Export all types
